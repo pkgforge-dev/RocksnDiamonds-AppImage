4df2a36a13cddb15bdf64e6e92a421213d9ef565
Link4Electronics <link4electronics@gmail.com>
2026-04-07 03:14:47 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,22 +3,24 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q rocksndiamonds | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/128x128/apps/rocksndiamonds.png
+export DESKTOP=/usr/share/applications/rocksndiamonds.desktop
+export STARTUPWMCLASS=rocksndiamonds
+export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
-
-# Additional changes can be done in between here
+quick-sharun /usr/bin/rocksndiamonds /usr/share/rocksndiamonds \
+/usr/lib/libxmp.so* \
+/usr/lib/libmodplug.so*
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# Test the app for 12 seconds, if the test fails due to the app
-# having issues running in the CI use --simple-test instead
-quick-sharun --test ./dist/*.AppImage
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage

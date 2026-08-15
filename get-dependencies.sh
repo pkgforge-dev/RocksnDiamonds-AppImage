2b477e73f9c57ba4434abf3ad51b9a1873719097
Link4Electronics <link4electronics@gmail.com>
2026-08-15 05:44:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,10 +14,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 if [ "${ARCH}" = x86_64 ]; then
     pacman -S --noconfirm rocksndiamonds
 else

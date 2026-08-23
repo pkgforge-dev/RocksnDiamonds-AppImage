d8adcc38409af6bdbb9a941924bc23caa8a4ced9
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:39:57 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/rocksndiamonds.png
+export ICON=/usr/share/icons/hicolor/128x128/apps/rocksndiamonds.png
 export DESKTOP=/usr/share/applications/rocksndiamonds.desktop
 export STARTUPWMCLASS=rocksndiamonds
 export DEPLOY_OPENGL=1

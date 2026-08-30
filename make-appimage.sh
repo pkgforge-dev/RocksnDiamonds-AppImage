e58ef56f1684237a01a2940f189ea9e9f4d92b67
Link4Electronics <link4electronics@gmail.com>
2026-08-30 05:49:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -20,3 +20,7 @@ quick-sharun /usr/bin/rocksndiamonds /usr/share/rocksndiamonds \
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --simple-test ./dist/*.AppImage
